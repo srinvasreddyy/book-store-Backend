0cc2dc1891ba0d0b96671335260ba407eac27a05
pranaygoud01 <pranaygoudpalle123@gmail.com>
2025-10-19 14:08:07 +0530

    Add sameSite cookie attribute for cross-origin support

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -135,6 +135,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   return res
@@ -160,6 +161,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   return res
@@ -206,6 +208,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const options = {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     };
 
     return res
